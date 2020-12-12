203b43b2ae82b45640260ba0eeee4f6e55b334c7
PexMor <p.moravek@xlinux.cz>
2020-12-12 13:09:50 +0100

    add event handler for installable app

--- sw.js
+++ sw.js
@@ -1,1 +1,13 @@
 // Empty worker so far
+window.addEventListener('beforeinstallprompt', (event) => {
+  console.log('👍', 'beforeinstallprompt', event);
+  // Stash the event so it can be triggered later.
+  window.deferredPrompt = event;
+  // Remove the 'hidden' class from the install button container
+  divInstall.classList.toggle('hidden', false);
+});
+
+window.addEventListener('appinstalled', (event) => {
+  console.log('👍', 'appinstalled', event);
+});
+
